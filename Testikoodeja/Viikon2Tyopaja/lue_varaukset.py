55eca7a33ca1c0d558afae8f27fc2f7c7b4a0b38
Ville Heikkiniemi <ville.heikkiniemi@gmail.com>
2025-11-05 17:28:54 +0200

    tyopaja 2

--- Testikoodeja/Viikon2Tyopaja/lue_varaukset.py
+++ Testikoodeja/Viikon2Tyopaja/lue_varaukset.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 """
 Ohjelma joka lukee tiedostossa olevat varaustiedot
 ja tulostaa ne konsoliin. Alla esimerkkitulostus:
@@ -25,20 +26,28 @@ def main():
         varaus = f.read().strip()
 
     # Tulostetaan varaus konsoliin
-    print(varaus)
-
-    # Kokeile näitä
-    #print(varaus.split('|'))
-    #varausId = varaus.split('|')[0]
-    #print(varausId)
-    #print(type(varausId))
-    """
-    Edellisen olisi pitänyt tulostaa numeron 123, joka
-    on oletuksena tekstiä.
-
-    Voit kokeilla myös vaihtaa kohdan [0] esim. seuraavaksi [1]
-    ja testata mikä muuttuu
-    """
+    varausnumero = int(varaus.split('|')[0])
+    print("Varausnumero:", varausnumero)
+    varaajannimi = varaus.split('|')[1]
+    print("Varaaja:", varaajannimi)
+    paivamaara = datetime.strptime(varaus.split('|')[2], "%Y-%m-%d").date()
+    print("Päivämäärä:", paivamaara.strftime("%d.%m.%Y"))
+    aloitusaika = datetime.strptime(varaus.split('|')[3], "%H:%M").time()
+    print("Aloitusaika:", aloitusaika.strftime("%H.%M"))
+    tuntimaara = int(varaus.split('|')[4])
+    print("Tuntimäärä:", tuntimaara)
+    tuntihinta = float(varaus.split('|')[5])
+    print("Tuntihinta:", tuntihinta, "€")
+    kokonaishinta = tuntihinta*tuntimaara
+    print("Kokonaishinta:", kokonaishinta, "€")
+    maksettu = varaus.split('|')[6]
+    print(f"Maksettu: {'Kyllä' if maksettu else 'Ei'}")
+    varauskohde = varaus.split('|')[7]
+    print("Kohde:", varauskohde)
+    puhelinnumero = varaus.split('|')[8]
+    print("Puhelin:", puhelinnumero)
+    sahkoposti = varaus.split('|')[9]
+    print("Sähköposti:", sahkoposti)
 
 if __name__ == "__main__":
     main()
